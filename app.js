5618553a76c52da6c4c4e8c7582db032c39a9c05
jayesh-borase <borsejayesh069@gmail.com>
2025-09-05 18:26:40 +0530

    added routes for index.ejs,show.ejs,new.ejs

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path"); 
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));
 
 main()
 .then((res) => console.log("db connect.."))
@@ -9,9 +15,23 @@ main()
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-
-  
 }
+app.get("/listings",async (req,res) => {
+  const allListings=await Listing.find();
+  res.render("./listings/index.ejs",{allListings});
+})
+
+app.get("/listings/new",(req,res) => {
+  res.render("/listings/new.ejs");
+})
+
+
+app.get("/listings/:id",async (req,res) => {
+  const { id }= req.params;
+  const listing=await Listing.findById(id);
+  res.render("./listings/show.ejs",{listing});
+})
+
 app.listen(8080,()=>{
     console.log("app listning on port 8080");
 })
